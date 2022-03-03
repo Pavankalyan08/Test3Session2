7adc12b484325e480ec6b2a7c74c74e3ae7bf636
Pavankalyan08 <96220569+Pavankalyan08@users.noreply.github.com>
2022-03-03 08:53:11 +0000

    p7final

--- p7final.c
+++ p7final.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#include<math.h>
+struct _fraction
+{
+  int num,den;
+};
+typedef struct _fraction fraction;
+int find_gcd(int a,int b)
+{
+  int gcd(int a,int b)
+  {
+    int t;
+    while(b!=0)
+      {
+        t=b;
+        b=a%b;
+        a=t;
+      }
+    return a;
+  }
+  int main()
+  {
+    printf("%d\n",gcd(16,24));
+  }
+}
+    
 
