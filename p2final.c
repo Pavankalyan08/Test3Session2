4ff8d4369fe6d4fd45f8f52c311cc588b1a7d501
Pavankalyan08 <96220569+Pavankalyan08@users.noreply.github.com>
2022-03-08 17:46:53 +0000

    p2 final

--- p2final.c
+++ p2final.c
@@ -1,1 +1,14 @@
-
+#include<stdio.h>
+#include<math.h>
+struct_fraction
+{
+  int f1;
+  int f2;
+  int f3;
+};
+typedef struct fraction 1, fraction 2,fraction 3;
+fraction input_fraction()
+{
+  printf("enter the three fractions\n");
+  scanf("")
+}
